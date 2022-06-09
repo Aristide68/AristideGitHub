3bde8c3f6221f16fda29f3d53ee44fc1e10a37d2
rosam <rosam@LAPTOP-J9NR809C>
2022-06-09 16:45:21 +0200

    Inserita clase pizza

---
+++ src/main/java/com/acme/Milazz/Pizza.java
@@ -0,0 +1,14 @@
+package com.acme.Milazz;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Pizza {
+ private String prezzo;
+ private int calorie;
+	
+}
