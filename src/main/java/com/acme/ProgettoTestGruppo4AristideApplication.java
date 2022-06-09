9e5244a72dd07cc98fc69b535f18e433137df76a
arist <arist@ARISTIDE.homenet.telecomitalia.it>
2022-06-09 16:21:04 +0200

    ProjectAristide

---
+++ src/main/java/com/acme/ProgettoTestGruppo4AristideApplication.java
@@ -0,0 +1,13 @@
+package com.acme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoTestGruppo4AristideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoTestGruppo4AristideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acme/ProgettoTestGruppo4AristideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoTestGruppo4AristideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
